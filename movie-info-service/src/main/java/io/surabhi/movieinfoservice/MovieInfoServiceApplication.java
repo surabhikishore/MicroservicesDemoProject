e32722d4ecadd0cee35ff7b647547a096aecaec5
surabhikishore <83238604+surabhikishore@users.noreply.github.com>
2022-06-20 00:01:23 +0530

    movie-info-service

---
+++ movie-info-service/src/main/java/io/surabhi/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,15 @@
+package io.surabhi.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
